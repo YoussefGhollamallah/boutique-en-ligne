3214f5a6fa2d06fdf1b51f2847f988175ecb21a6
lucas-iribaren <lucas.iribaren@laplateforme.io>
2024-10-22 16:11:23 +0200

    Refactor panier.php to improve button styling and add CSS classes

--- src/views/panier.php
+++ src/views/panier.php
@@ -6,26 +6,26 @@
                 <img src="creeper.png" alt="Peluche creeper">
                 <p>Peluche creeper 1 : 13.50€</p>
                 <input type="number" value="1" min="1">
-                <button class="btn-supprimer">Supprimer</button>
+                <button class="btn btn-supprimer">Supprimer</button>
             </section><br>
             <section>
                 <input type="checkbox">
                 <img src="creeper.png" alt="Peluche creeper">
                 <p>Peluche creeper 2 : 13.50€</p>
                 <input type="number" value="1" min="1">
-                <button class="btn-supprimer">Supprimer</button>
+                <button class="btn btn-supprimer">Supprimer</button>
             </section><br>
             <section>
                 <input type="checkbox">
                 <img src="creeper.png" alt="Peluche creeper">
                 <p>Peluche creeper 3 : 13.50€</p>
                 <input type="number" value="1" min="1">
-                <button class="btn-supprimer">Supprimer</button>
+                <button class="btn btn-supprimer">Supprimer</button>
             </section><br>
             <section>
                 <p>3 articles dans le panier</p>
                 <p>Total de la commande : 27€ (2 articles)</p>
-                <button class="btn-ajouter">Valider la commande</button>
+                <button class="btn btn-ajouter">Valider la commande</button>
             </section>
     </section>
 </main>

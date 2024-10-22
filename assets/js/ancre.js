3dd3a855c0b60c1f3b777914ee0c13f19317e115
lucas-iribaren <lucas.iribaren@laplateforme.io>
2024-10-22 16:08:28 +0200

    Refactor ancre.js to improve scroll behavior and anchor link functionality

--- assets/js/ancre.js
+++ assets/js/ancre.js
@@ -1,3 +1,14 @@
+window.addEventListener('scroll', function () {
+    const ancre = document.querySelector('.ancre');
+    if (window.scrollY > 1500) {
+        ancre.style.opacity = '1';
+        ancre.style.pointerEvents = 'auto';  // Pour activer le clic
+    } else {
+        ancre.style.opacity = '0';
+        ancre.style.pointerEvents = 'none';  // Désactive le clic quand l'ancre est invisible
+    }
+});
+
 document.querySelector('.ancre').addEventListener('click', function (e) {
     e.preventDefault(); // Empêche le comportement par défaut du lien
 
@@ -6,7 +17,7 @@ document.querySelector('.ancre').addEventListener('click', function (e) {
 
     if (targetElement) {
         targetElement.scrollIntoView({
-            behavior: 'smooth' // Ajoute l'effet smooth scrolling
+            behavior: 'smooth' 
         });
     }
 });

a732aae3bcec09905d6d6c939642664f6460a762
lucas-iribaren <lucas.iribaren@laplateforme.io>
2024-10-23 09:49:45 +0200

    Refactor index.php to display the last three products in a carousel

--- src/views/index.php
+++ src/views/index.php
@@ -1,7 +1,9 @@
 <?php
+include 'src/controllers/ProduitController.php';
 
 $produitController = new ProduitController();
 $products = $produitController->getAllProducts();
+$lastThreeProducts = $produitController->getLastThreeProducts();
 
 ?>
 
@@ -9,47 +11,32 @@ $products = $produitController->getAllProducts();
     <h3>Produits phares</h3>
     <article class="article_phare flex space-around carousel slide" data-ride="carousel">
         <div class="carousel-inner">
-            <div class="carousel-item active">
-                <div class="product-details">
-                    <img src="<?php echo ASSETS; ?>/images/placeholder.png" alt="Produit 1">
-                    <div class="carousel-caption">
-                        <h5>
-                            Produit 1
-                        </h5>
-                        <p>
-                            Peluche représentant Izuku Midoriya (Deku), l'apprenti héros de My Hero Academia, dans son costume vert.
-                        </p>
-                    </div>
-                </div>
-                <div class="carousel-item">
+            <?php
+            $isActive = true;
+            foreach ($lastThreeProducts as $produit) {
+                $nomProduit = ucwords(str_replace('_', ' ', $produit['nom']));
+                $activeClass = $isActive ? 'active' : '';
+                $isActive = false;
+            ?>
+                <div class="carousel-item <?php echo $activeClass; ?>">
                     <div class="product-details">
-                        <img src="./assets/images/placeholder.png" alt="Produit 1">
+                        <img src="assets/images/<?php echo $produit['image']; ?>" alt="Produit <?php echo $nomProduit; ?>">
                         <div class="carousel-caption">
-                            <h5>
-                            </h5>
-                            <p>
-                            </p>
-                        </div>
-                    </div>
-                    <div class="carousel-item">
-                        <div class="product-details">
-                            <img src="./assets/images/placeholder.png" alt="Produit 3">
-                            <div class="carousel-caption">
-                                <h5>
-                                </h5>
-                                <p>
-                                </p>
-                            </div>
+                            <h5><?php echo $nomProduit; ?></h5>
+                            <p><?php echo $produit['description']; ?></p>
                         </div>
                     </div>
                 </div>
-                <a class="carousel-control-prev" href="#carouselPharePrev" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                </a>
-                <a class="carousel-control-next" href="#carouselPhareNext" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                </a>
-                <a href="#carouselPhareDetail" class="carousel-control-product"></a>
+            <?php
+            }
+            ?>
+        </div>
+        <a class="carousel-control-prev" href="#carouselPharePrev" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        </a>
+        <a class="carousel-control-next" href="#carouselPhareNext" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        </a>
     </article>
 </section>
 <section class="section_categorie   flex column ">

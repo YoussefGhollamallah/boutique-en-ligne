d65dbea23a6fed440f2c0939b6e44081f31d9a85
YoussefGhollamallah <youssef.gh@live.fr>
2024-10-27 21:08:22 +0100

    mise en place de la différienciation de la page admin et clien par la récupération du role_id

--- src/views/connexion.php
+++ src/views/connexion.php
@@ -1,5 +1,8 @@
 <?php
 
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
 require_once __DIR__ . '/../controllers/UtilisateurController.php';
 
 $user = new UtilisateurController();
@@ -23,8 +26,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         }
     } elseif ($action === 'login') {
         if (!empty($email) && !empty($password)) {
-            if ($user->userConnexion($email, $password)) {
-                header('Location: /profil');
+            $userData = $user->userConnexion($email, $password);
+            if ($userData) {
+                $_SESSION['user'] = $userData;
+                header('Location: profil');
                 exit();
             } else {
                 echo "Email ou mot de passe incorrect.";
@@ -34,12 +39,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         }
     }
 }
+
+if (isset($_SESSION['user'])) {
+    header('Location: profil');
+    exit();
+}
 ?>
 <main class="main-form">
-
-    
-    <div class="container-form ">
+    <div class="container-form">
         <div class="form-container" id="register-form">
+            <button class="btn btn-primary form-button" onclick="switchForm('login')">Déjà un compte? Connectez-vous</button>
             <h2>Inscription</h2>
             <form class="form" action="" method="post">
                 <input type="hidden" name="action" value="register">
@@ -47,57 +56,50 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                     <label for="prenom">Prénom</label>
                     <input type="text" name="prenom" id="prenom" required>
                 </div>
-
-                <div> 
+                <div>
                     <label for="nom">Nom</label>
                     <input type="text" name="nom" id="nom" required>
                 </div>
-
                 <div>
                     <label for="email">Email</label>
                     <input type="email" name="email" id="email" required>
                 </div>
-
                 <div>
                     <label for="password">Mot de passe</label>
                     <br>
                     <input type="password" name="password" id="password" required>
-                    
                 </div>
                 <button class="btn btn-ajouter" type="submit">S'inscrire</button>
             </form>
-            <button class="btn btn-primary form-button" onclick="switchForm('login')">Déjà un compte? Connectez-vous</button>
         </div>
 
         <div class="form-container" id="login-form" style="display: none;">
+            <button class="btn btn-primary form-button" onclick="switchForm('register')">Pas encore de compte? Inscrivez-vous</button>
             <h2>Connexion</h2>
             <form action="" class="form" method="post">
-                
                 <input type="hidden" name="action" value="login">
                 <div>
                     <label for="email">Email</label>
                     <input type="email" name="email" id="email" required>
                 </div>
                 <div>
-                    
                     <label for="password">Mot de passe</label>
                     <input type="password" name="password" id="password" required>
                 </div>
                 <button class="btn btn-ajouter" type="submit">Se connecter</button>
             </form>
-            <button class="btn btn-primary form-button" onclick="switchForm('register')">Pas encore de compte? Inscrivez-vous</button>
         </div>
     </div>
 </main>
 
-    <script>
-        function switchForm(form) {
-            if (form === 'register') {
-                document.getElementById('register-form').style.display = 'block';
-                document.getElementById('login-form').style.display = 'none';
-            } else if (form === 'login') {
-                document.getElementById('register-form').style.display = 'none';
-                document.getElementById('login-form').style.display = 'block';
-            }
+<script>
+    function switchForm(form) {
+        if (form === 'register') {
+            document.getElementById('register-form').style.display = 'block';
+            document.getElementById('login-form').style.display = 'none';
+        } else if (form === 'login') {
+            document.getElementById('register-form').style.display = 'none';
+            document.getElementById('login-form').style.display = 'block';
         }
-    </script>
+    }
+</script>
--- src/views/profil.php
+++ src/views/profil.php
@@ -1,1 +1,53 @@
-<p>Mon profil</p>
+<?php
+
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+if (!isset($_SESSION['user'])) {
+    header('Location: connexion');
+    exit;
+}
+
+
+?>
+
+<main>
+    <?php if (isset($_SESSION["user"]["role_id"]) && $_SESSION["user"]["role_id"] == 2) : ?>
+        <section>
+            <h2>Mon Profil</h2>
+        </section>
+        <section>
+            <h3>Mes informations</h3>
+            <div class="profil">
+                <div>
+                    <p>Prénom : <?php echo $_SESSION['user']['prenom']; ?></p>
+                    <p>Nom : <?php echo $_SESSION['user']['nom']; ?></p>
+                    <p>Email : <?php echo $_SESSION['user']['email']; ?></p>
+                </div>
+            </div>
+        </section>
+    <?php elseif (isset($_SESSION["user"]["role_id"]) && $_SESSION["user"]["role_id"] == 1) : ?>
+        <section>
+            <h2>Mon Profil</h2>
+        </section>
+        <section>
+            <h3>Mes informations</h3>
+            <div class="profil">
+                <div>
+                    <p>Prénom : <?php echo $_SESSION['user']['prenom']; ?></p>
+                    <p>Nom : <?php echo $_SESSION['user']['nom']; ?></p>
+                    <p>Email : <?php echo $_SESSION['user']['email']; ?></p>
+                </div>
+            </div>
+        </section>
+        <section>
+            <h3>Mon rôle</h3>
+            <div class="profil">
+                <div>
+                    <p>Rôle : Administrateur</p>
+                </div>
+            </div>
+        </section>
+    <?php endif; ?>
+</main>

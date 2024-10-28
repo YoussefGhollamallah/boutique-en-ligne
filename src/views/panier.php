9ecd1bebc256364b2c0ce994576ab926e761c946
lucas-iribaren <lucas.iribaren@laplateforme.io>
2024-10-28 17:00:26 +0100

    Refactor panier.php to add styling for the shopping cart section

--- src/views/panier.php
+++ src/views/panier.php
@@ -41,26 +41,30 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 }
 ?>
 
-<section class="section">
+<section class="section ">
     <h3>Votre Panier</h3>
-    <?php if (!empty($panier)) { ?>
-        <?php foreach ($panier as $idProduit => $produit) {
-            $checked = isset($produit['checked']) ? $produit['checked'] : false; ?>
-            <div class="card_produit" id="produit_<?php echo htmlspecialchars($idProduit); ?>">
-                <img class="card_produit_img" src="assets/images/<?php echo htmlspecialchars($produit['image']); ?>" alt="<?php echo htmlspecialchars($produit['nom']); ?>">
-                <h4>
-                    <input type="checkbox" class="produit-checkbox" data-id="<?php echo htmlspecialchars($idProduit); ?>" <?php echo $checked ? 'checked' : ''; ?>>
-                    <?php echo htmlspecialchars($produit['nom']); ?>
-                </h4>
-                <p><?php echo htmlspecialchars($produit['description']); ?></p>
-                <p>Prix unitaire : <span class="prix-produit"><?php echo htmlspecialchars($produit['prix']); ?></span> €</p>
-                <p>Quantité :</p>
-                <input type="number" value="<?php echo intval($produit['quantite']); ?>" min="1" max="<?php echo intval($produit['quantite_max']); ?>" class="quantite-input" data-id="<?php echo htmlspecialchars($idProduit); ?>">
-                <p>Total : <span class="produit-total"><?php echo htmlspecialchars($produit['prix'] * $produit['quantite']); ?> €</span></p>
-                <button class="btn btn-supprimer" data-id="<?php echo intval($produit['id']); ?>">Supprimer</button>
-            </div>
-        <?php } ?>
+    
+    <aside class="flex space-around ">
 
+        <?php if (!empty($panier)) { ?>
+            <?php foreach ($panier as $idProduit => $produit) {
+                $checked = isset($produit['checked']) ? $produit['checked'] : false; ?>
+                <div class="" id="produit_<?php echo htmlspecialchars($idProduit); ?>">
+                    <img class="card_produit_img bg-backgrou" src="assets/images/<?php echo htmlspecialchars($produit['image']); ?>" alt="<?php echo htmlspecialchars($produit['nom']); ?>">
+                    <h4>
+                        <input type="checkbox" class="produit-checkbox" data-id="<?php echo htmlspecialchars($idProduit); ?>" <?php echo $checked ? 'checked' : ''; ?>>
+                        <?php echo htmlspecialchars($produit['nom']); ?>
+                    </h4>
+                    <p><?php echo htmlspecialchars($produit['description']); ?></p>
+                    <p>Prix unitaire : <span class="prix-produit"><?php echo htmlspecialchars($produit['prix']); ?></span> €</p>
+                    <p>Quantité :</p>
+                    <input type="number" value="<?php echo intval($produit['quantite']); ?>" min="1" max="<?php echo intval($produit['quantite_max']); ?>" class="quantite-input" data-id="<?php echo htmlspecialchars($idProduit); ?>">
+                    <p>Total : <span class="produit-total"><?php echo htmlspecialchars($produit['prix'] * $produit['quantite']); ?> €</span></p>
+                    <button class="btn btn-supprimer" data-id="<?php echo intval($produit['id']); ?>">Supprimer</button>
+                </div>
+            <?php } ?>
+    
+        </aside>
         <h4>Total du panier : <span id="total-panier"><?php echo number_format($totalPanier, 2, ',', ' '); ?> €</span></h4>
         <button class="btn btn-ajouter">Valider la commande</button>
     <?php } else { ?>
